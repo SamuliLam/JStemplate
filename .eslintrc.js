4b8f9c7b3c9a00dfd8f082529b41d3df3b86b01a
Samuli Lamminmäki <36449245+SamuliLam@users.noreply.github.com>
2024-03-25 10:26:18 +0200

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,28 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['google', 'eslint:recommended', 'plugin:prettier/recommended'], 'airbnb-base',
+  extends: [
+    "google",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "airbnb-base",
+  ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'require-jsdoc': 0,
+    "require-jsdoc": 0,
   },
 };
